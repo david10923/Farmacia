9eed2009b4c8da9e54e29d4c7da73538295f3d47
David <davidf14@ucm.es>
2020-03-23 14:05:14 +0100

    Añadiendo codigo

---
+++ src/Controller/ControllerImp.java
@@ -0,0 +1,23 @@
+package Controller;
+
+import Presentacion.Vista.GUIFarmacia;
+import Presentacion.Vista.IGUI;
+
+public class ControllerImp extends Controller{
+	
+	private IGUI igui;
+	private GUIFarmacia farmacia;
+	
+	
+	
+	public ControllerImp(){
+		
+	}
+
+	@Override
+	public void action(Object data, Integer event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/Controller/Evento.java
@@ -0,0 +1,23 @@
+package Controller;
+
+public class Evento {
+	
+	///////////////////////////////////CLIENTE/////////////////////////////// 
+	public static final int ALTA_CLIENTE= 100; 
+	public static final int BAJA_CLIENTE= 101; 
+	public static final int MODIFICAR_CLIENTE = 102;
+	public static final int MOSTRAR_CLIENTE = 103; 
+	public static final int MOSTRAR_TODOS_CLIENTES = 104; 
+	public static final int RES_ALTA_CLIENTE_OK =105; 
+	public static final int RES_ALTA_CLIENTE_FAILED= -105;
+	public static final int RES_BAJA_CLIENTE_OK=106;
+	public static final int RES_BAJA_CLIENTE_FAILED = -106;
+	public static final int RES_MODIFICAR_CLIENTE_OK = 107;
+	public static final int RES_MODIFICAR_CLIENTE_FAILED= -107;
+	public static final int RES_MOSTRAR_CLIENTES_OK=108;
+	public static final int	RES_MOSTRAR_CLIENTES_FAILED =-108;
+	public static final int RES_MOSTRAR_TODOS_CLIENTES_OK=109;
+	public static final int	RES_MOSTRAR_TODOS_CLIENTES_FAILED =-109;
+	
+
+}
