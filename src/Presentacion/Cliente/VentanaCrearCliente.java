3c8c946921d60651eb8f5e244c719b2acd60b736
David <davidf14@ucm.es>
2020-03-29 20:26:45 +0200

    Organizada en codigo la ventana de dar de alta

--- src/Presentacion/Cliente/VentanaCrearCliente.java
+++ src/Presentacion/Cliente/VentanaCrearCliente.java
@@ -24,100 +24,73 @@ import Tranfers.TCliente;
 
 public class VentanaCrearCliente extends JDialog implements ActionListener {
 	
-	JLabel DNI_Label;
-	JTextField DNI_Text;
-	JLabel TARJETA_Label;
-	JTextField TARJETA_Text;
-	JLabel NOMBRE_Label;
-	JTextField NOMBRE_Text;
-	JButton botonAceptar;
-	JButton botonCancelar;
+	private JLabel DNI_Label; 
+	private JTextField DNI_Text;
+	private JLabel TARJETA_Label;
+	private JTextField TARJETA_Text;
+	private JLabel NOMBRE_Label;
+	private JTextField NOMBRE_Text;
+	private JButton botonAceptar;
+	private JButton botonCancelar;
 	
 	public VentanaCrearCliente(){
 		
-		this.setBounds(350, 100,300,400);
-
-		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		iniciarventana();
 		
-		this.setLayout(null);
-
 		
-		NOMBRE_Label = new JLabel("NOMBRE: ");
-		
-		NOMBRE_Label.setBounds(10, 120, 165, 25);
-		
-		this.add(NOMBRE_Label);
-		
-		NOMBRE_Text = new JTextField();
-		
-		NOMBRE_Text.setBounds(100, 120, 165, 25);
-		
+		/////NOMBRE //////		
+		NOMBRE_Label = new JLabel("NOMBRE: ");		
+		NOMBRE_Label.setBounds(10, 120, 165, 25);		
+		this.add(NOMBRE_Label);		
+		NOMBRE_Text = new JTextField();		
+		NOMBRE_Text.setBounds(100, 120, 165, 25);		
 		this.add(NOMBRE_Text);
 		
-		DNI_Label = new JLabel("DNI: ");
 		
-		DNI_Label.setBounds(10, 20, 80, 25);
-		
-		this.add(DNI_Label);
-		
-		DNI_Text = new JTextField();
-		
-		DNI_Text.setBounds(100, 20, 165, 25);
-		
+		////////DNI////////
+		DNI_Label = new JLabel("DNI: ");		
+		DNI_Label.setBounds(10, 20, 80, 25);		
+		this.add(DNI_Label);		
+		DNI_Text = new JTextField();		
+		DNI_Text.setBounds(100, 20, 165, 25);		
 		this.add(DNI_Text);
 		
-		TARJETA_Label = new JLabel("TARJETA SANITARIA: ");
 		
-		TARJETA_Label.setBounds(10, 70, 80, 25);
-		
-		this.add(TARJETA_Label);
-		
-		TARJETA_Text = new JTextField();
-		
-		TARJETA_Text.setBounds(100, 70, 165, 25);
-		
+		///////TARJETA SANITARIA////////		
+		TARJETA_Label = new JLabel("TARJETA SANITARIA: ");		
+		TARJETA_Label.setBounds(10, 70, 80, 25);		
+		this.add(TARJETA_Label);		
+		TARJETA_Text = new JTextField();		
+		TARJETA_Text.setBounds(100, 70, 165, 25);		
 		this.add(TARJETA_Text);
-
 		this.add(Box.createRigidArea(new Dimension(5,5)));
 		
+		
+		//////BOTON DE ACEPTAR/////
 		botonAceptar = new JButton("Aceptar");
 		botonAceptar.setBounds(80, 160, 80, 25);
-		botonAceptar.addActionListener(this);
-		
+		botonAceptar.addActionListener(this);		
 		this.add(botonAceptar);
 		
-		
+		///////BOTON DE CANCELAR////////
 		botonCancelar = new JButton("Cancelar");
 		botonCancelar.setBounds(170, 160, 90, 25);
-		botonCancelar.addActionListener(this);		
-		
-		
+		botonCancelar.addActionListener(this);			
 		this.add(botonCancelar);
 		
-		this.setVisible(true);
-		//iniciarventana();
+		
+		
 	}
 	
 	void iniciarventana(){
 		
-		
-		
-		this.setBounds(350, 200, 70,70);
+		this.setBounds(350, 100,300,400);
 		this.setVisible(true);
-		this.setDefaultCloseOperation(EXIT_ON_CLOSE);//salta exception aqui
-		this.setLayout(new BorderLayout());
+		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		this.setLayout(null);
 		this.setTitle("Alta Cliente");
-		
-		iniciarComponentes();
-		
 	}
 	
-	void iniciarComponentes(){
-		JTextField id = new JTextField();
-		JLabel etiquetaID = new JLabel ();
-		this.add(etiquetaID);
-		this.add(id);
-	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
@@ -130,10 +103,9 @@ public class VentanaCrearCliente extends JDialog implements ActionListener {
 			Controller.getInstance().action(aux, Evento.ALTA_CLIENTE);
 			
 			removeBox();
-		}else if (botonPulsado == botonCancelar) {
 			
-			setVisible(false);
-			
+		}else if (botonPulsado == botonCancelar) {			
+			setVisible(false);			
 			dispose();
 			
 		}
